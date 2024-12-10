d13a7cb5b1e6e2ae44016deed7880dabbd09093f
Siddhant Nautiyal <71310931+WatashiwaSid@users.noreply.github.com>
2024-12-10 20:59:53 +0530

    default constructor

---
+++ defaultcon.cs
@@ -0,0 +1,18 @@
+using System;
+
+//default constructor
+public class Constructors
+{
+    public Constructors()
+    {
+        Console.WriteLine("Default constructor invoked.");
+    }
+}
+
+public class MainClass
+{
+    public static void Main(String[] __)
+    {
+        Constructors obj = new Constructors();
+    }
+}
